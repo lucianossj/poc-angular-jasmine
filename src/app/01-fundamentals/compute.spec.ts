259faaf9b2fe2bec61e7ebf8bd982a8c60e380f7
Luciano da Silva Sanrtos Junior <lsjunior@desenvvar31-lnx.dimed.local>
2019-07-30 10:00:00 -0300

    Unit Testing Fundamentals

---
+++ src/app/01-fundamentals/compute.spec.ts
@@ -0,0 +1,21 @@
+import { compute } from './compute';
+
+describe('compute', () => {
+
+  it('Should return 0 if the input is negative ', () => {
+
+    const result = compute(-1);
+
+    expect(result).toBe(0);
+
+  });
+
+  it('Should increment the input if it is positive', () => {
+
+    const result = compute(10);
+
+    expect(result).toBe(11);
+
+  });
+
+});
