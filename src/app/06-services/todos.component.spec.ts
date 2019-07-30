040c165b9a623a4454041e3d46fbf7835a75fa40
Luciano S. Junior <luciano.ssj@hotmail.com>
2019-07-30 13:51:36 -0300

    Interaction Testing

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,7 +1,10 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/from'
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
+
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -34,4 +37,43 @@ describe('TodosComponent', () => {
     });
 
   });
+
+  it('Should call the server to save the cnahges whena a new todo item is added', () => {
+    
+    let spy = spyOn(service, 'add').and.callFake(t => {
+
+      return Observable.empty();
+
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+
+  });
+
+  it('Should add the new todo returned from the server', () => {
+    
+    let todo = { id: 1 };
+
+    let spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+    
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+
+  });
+
+  it('Should set the message property if server returns an error when adding a new todo', () => {
+    
+    let error = 'error from the server';
+
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw() ]));
+    
+    component.add();
+
+    expect(component.message).toBe(error);
+
+  });
+
 });
