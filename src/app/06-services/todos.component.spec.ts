de9061d061f20b82172e71b8227019c921515d1a
Luciano S. Junior <luciano.ssj@hotmail.com>
2019-07-30 11:48:37 -0300

    Working with Spies

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,37 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from'
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+
+    service = new TodoService(null);
+
+    component = new TodosComponent(service);
+
   });
 
-  it('', () => {
+  it('Should set todos property the items returned from the server', () => {
+
+    let todos = [
+        {id: 1, title: 'a'},
+        {id: 2, title: 'b'},
+        {id: 3, title: 'c'}
+      ];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+
+      return Observable.from([todos]);
+
+      component.ngOnInit();
+
+      expect(component.todos.length).toBe(todos);
+
+    });
+
   });
 });
